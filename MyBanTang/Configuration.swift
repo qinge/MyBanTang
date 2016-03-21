3ac277db1c0394370cae799c1701e45bc61249c9
qinge <378144946@qq.com>
2016-03-21 18:03:33 +0800

    add welcome page and top page frame

---
+++ MyBanTang/Configuration.swift
@@ -0,0 +1,9 @@
+//
+//  Configuration.swift
+//  MyBanTang
+//
+//  Created by snqu-ios on 16/3/21.
+//  Copyright © 2016年 snqu-ios. All rights reserved.
+//
+
+import Foundation
---
+++ MyBanTang/MainTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarController.swift
+//  MyBanTang
+//
+//  Created by snqu-ios on 16/3/21.
+//  Copyright © 2016年 snqu-ios. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
