bda4a95a951f4e0049ee467407f6bd1c666d11df
qinge <378144946@qq.com>
2016-03-21 18:04:41 +0800

    config global configuration

--- MyBanTang/ViewController.swift
+++ MyBanTang/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer: NSTimer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let delayInSeconds = 2.0
+        let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( delayInSeconds * Double(NSEC_PER_SEC)))
+        dispatch_after(popTime, dispatch_get_main_queue()) { () -> Void in
+            let window = UIApplication.sharedApplication().delegate!.window!
+            window!.rootViewController = self.resolveNextViewController()
+        }
+    }
+    
+    func resolveNextViewController() -> UIViewController {
+        if NSUserDefaults.standardUserDefaults().objectForKey("FirstStart") != nil {
+            return MainTabBarController()
+        }else {
+            NSUserDefaults.standardUserDefaults().setObject("FirstStart", forKey: "FirstStart")
+            return WelcomeViewController()
+        }
+    }
 
 }
 
